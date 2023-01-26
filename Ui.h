87ce334f3ae3e7bb48810a22a1fdf1481a05dd64
ColEas21 <80491029+ColEas21@users.noreply.github.com>
2023-01-26 15:39:44 -0500

    Add files via upload

---
+++ Ui.h
@@ -0,0 +1,14 @@
+#include "Ui.cpp"
+#ifndef FINALPROJECT_UI_H
+#define FINALPROJECT_UI_H
+void enterTeamName();
+void printDraftQuestion();
+std::vector<std::string> convertArrayToVec(std::array<std::string, 3> myArray);
+std::vector<std::string> updateVec(std::vector<std::string> Rd1Vec, std::vector<std::string> Rd2Vec);
+void printVector(std::vector<std::string> myVec1);
+void tradeQuest();
+bool tradeCheck(std::string userAns1);
+void tradePlayeroffer();
+void desiredPlayerRec();
+void TradeAcceptQ();
+#endif //FINALPROJECT_UI_H
